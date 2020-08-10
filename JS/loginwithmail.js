74db2d10e97a9ce8fe39f7cd5e3f695d39ec91c4
priyanka <priyanka@91social.com>
2020-08-10 18:25:01 +0530

    nothing

--- JS/loginwithmail.js
+++ JS/loginwithmail.js
@@ -1,7 +1,12 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "30%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.6)";
+    document.getElementById("use").style.backgroundColor =  "rgba(0,0,0,0.3)";
+
 
 }
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = "white";
+    document.getElementById("use").style.backgroundColor =  "orange";
 }
